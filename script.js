a6e97f2ecfa265709fde567b0c8553e62d116688
Pham Quang Huy <p.q.huy97@gmail.com>
2018-05-12 12:34:06 +0700

    remove console log

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ $(document).ready( function() {
 
     $("#getData").click( function() {
         var htmlString = $("#editor").summernote('code');
-        // console.log(htmlStrin    g);
         $("#display").html(htmlString);
     })
 });
